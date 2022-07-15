6057c34252a4727f5001603ff5b0b70f5acf5859
DMKuldeep <95953606+DMKuldeep@users.noreply.github.com>
2022-07-15 11:05:25 +0530

    deployed

---
+++ build/precache-manifest.4f7355407d115e200cb68edffa7332c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf79cf87c981268cbaa0",
+    "url": "/trotoise/static/css/main.8418a545.chunk.css"
+  },
+  {
+    "revision": "cf79cf87c981268cbaa0",
+    "url": "/trotoise/static/js/main.cf79cf87.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/trotoise/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "9d703b4fa4613e4fb324",
+    "url": "/trotoise/static/js/2.9d703b4f.chunk.js"
+  },
+  {
+    "revision": "d72a939515f84d5fe7e4",
+    "url": "/trotoise/static/js/runtime~main.d72a9395.js"
+  },
+  {
+    "revision": "15409e91dc6e83e7e95f74d49d279fd9",
+    "url": "/trotoise/index.html"
+  }
+];
